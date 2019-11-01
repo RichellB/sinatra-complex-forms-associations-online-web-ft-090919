49066d2616daad9b84bffe4d83e500013edba4fa
Richell Bonfield <richell.bonfield12@gmail.com>
2019-11-01 00:48:49 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -20,15 +20,23 @@ class OwnersController < ApplicationController
 
   get '/owners/:id/edit' do 
     @owner = Owner.find(params[:id])
+    @pets = Pet.all
     erb :'/owners/edit'
   end
 
-  get '/owners/:id' do 
+  get '/owners/:id' do
     @owner = Owner.find(params[:id])
     erb :'/owners/show'
   end
 
-  patch '/owners/:id' do 
-   
+  patch '/owners/:id' do
+    @owner = Owner.find(params[:id])
+    @owner.update(params[:owner])
+
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+
+    redirect "/owners/#{@owner.id}"
   end
 end
